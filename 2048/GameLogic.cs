d6540b3cb2e40535e75b029c215cb870365ce0ad
Tomáš Kocůrek <tomas.kocurek2000@gmail.com>
2020-08-31 13:30:01 +0200

    Add project files.

---
+++ 2048/GameLogic.cs
@@ -0,0 +1,418 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace _2048
+{
+    /// <summary>
+    /// Class which handle game logic
+    /// </summary>
+    class GameLogic
+    {
+        public TextBlock[,] PlayingField { get; set; }
+        private int[,] PlayingMatrix { get; set; }
+
+        public GameLogic(TextBlock[,] playingField)
+        {
+            NewGame(playingField);
+        }
+
+        /// <summary>
+        /// Start new game
+        /// </summary>
+        /// <param name="playingField">Array with textblocks</param>
+        public void NewGame(TextBlock[,] playingField)
+        {
+            PlayingField = playingField;
+            PlayingMatrix = new int[4, 4];
+
+            //Populate PlayingMatrix wiht zeroes
+            for (int y = 0; y < PlayingField.GetLength(1); y++)
+            {
+                for (int x = 0; x < PlayingField.GetLength(0); x++)
+                {
+                    PlayingMatrix[x, y] = 0;
+                }
+            }
+
+            SyncFields();
+        }
+
+        /// <summary>
+        /// Sync PlayingField with data in PlayingMatrix
+        /// </summary>
+        private void SyncFields()
+        {
+            if (CheckEnd() == "lose")
+            {
+                MessageBox.Show("You lose!");
+                NewGame(PlayingField);
+                return;
+            }
+            if (CheckEnd() == "win")
+            {
+                MessageBox.Show("You win");
+                NewGame(PlayingField);
+                return;
+            }
+
+            SpawnNewTile();
+
+            //Load all tiles
+            for (int y = 0; y < PlayingField.GetLength(1); y++)
+            {
+                for (int x = 0; x < PlayingField.GetLength(0); x++)
+                {
+                    TextBlock item = PlayingField[x, y];
+
+                    switch (PlayingMatrix[x,y])
+                    {
+                        case 2:
+                            item.Background = Brushes.White;
+                            break;
+                        case 4:
+                            item.Background = Brushes.White;
+                            break;
+                        case 8:
+                            item.Background = Brushes.Orange;
+                            break;
+                        case 16:
+                            item.Background = Brushes.Orange;
+                            break;
+                        case 32:
+                            item.Background = Brushes.OrangeRed;
+                            break;
+                        case 64:
+                            item.Background = Brushes.OrangeRed;
+                            break;
+                        case 128:
+                            item.Background = Brushes.Yellow;
+                            break;
+                        case 256:
+                            item.Background = Brushes.Yellow;
+                            break;
+                        case 512:
+                            item.Background = Brushes.Yellow;
+                            break;
+                        case 1024:
+                            item.Background = Brushes.Yellow;
+                            break;
+                        case 2048:
+                            item.Background = Brushes.Yellow;
+                            break;
+                    }
+
+                    if (PlayingMatrix[x, y] != 0)
+                        item.Text = PlayingMatrix[x, y].ToString();
+                    else
+                    {
+                        item.Background = Brushes.LightGray;
+                        item.Text = "";
+                    }
+                }
+            }           
+        }
+
+        public int CountScore()
+        {
+            int sum = 0;
+
+            for (int y = 0; y < PlayingField.GetLength(1); y++)
+            {
+                for (int x = 0; x < PlayingField.GetLength(0); x++)
+                {
+                    sum += PlayingMatrix[x, y];
+                }
+            }
+
+            return sum;
+        }
+
+        /// <summary>
+        /// Spawn new tile in random positon
+        /// </summary>
+        private void SpawnNewTile()
+        {
+            bool valid = false;
+            Random random = new Random();
+            int x = 0;
+            int y = 0;
+
+            //Generate number until it finds free tile
+            while (valid == false)
+            {
+                x = random.Next(0, 4);
+                y = random.Next(0, 4);
+
+                if (PlayingMatrix[x, y] == 0)
+                    valid = true;
+            }
+
+            //Chance for spawning 4
+            if (random.Next(1, 11) == 1)
+                PlayingMatrix[x, y] = 4;
+            else
+                PlayingMatrix[x, y] = 2;
+        }
+
+        /// <summary>
+        /// Checking if win or defeate happended
+        /// </summary>
+        /// <returns>win/lose/""</returns>
+        private string CheckEnd()
+        {
+            string outcome = "";
+            bool freeSpace = false;
+
+            for (int y = 0; y < PlayingMatrix.GetLength(1); y++)
+            {
+                for (int x = 0; x < PlayingMatrix.GetLength(0); x++)
+                {
+                    //Check win
+                    if (PlayingMatrix[x, y] == 2048)
+                    {
+                        outcome = "win";
+                        break;
+                    }
+
+                    //Check deafet
+                    if (PlayingMatrix[x, y] == 0)
+                        freeSpace = true;
+                }
+            }
+
+            if ((outcome != "win") && (freeSpace))
+                return outcome;
+            else
+                return "lose";
+        }
+
+
+        /// <summary>
+        /// Move all possible blocks up
+        /// </summary>
+        public void UpMove()
+        {
+            //Make copy of playing matrix
+            int[,] temporaryMatrix = PlayingMatrix;
+            
+            //Check possible outcomes of the move
+            for (int y = 1; y < PlayingMatrix.GetLength(1); y++)
+            {
+                for (int x = 0; x < PlayingMatrix.GetLength(0); x++)
+                {
+                    if (temporaryMatrix[x, y] != 0)
+                        CheckUp(x, y, 1, temporaryMatrix, false);
+                }
+            }
+
+            //Copy new data into PlayingMatrix
+            PlayingMatrix = temporaryMatrix;
+            SyncFields();
+        }
+
+        /// <summary>
+        /// Check upper tiles and move current tile if is it possible
+        /// </summary>
+        /// <param name="x">originalX</param>
+        /// <param name="y">originalY</param>
+        /// <param name="i">currentOffset</param>
+        /// <param name="matrix">temporaryMatrix</param>
+        /// <param name="match">if tiles was match</param>
+        private void CheckUp(int x, int y, int i, int[,] matrix, bool match)
+        {
+            if (y - i >= 0)
+            {
+                //If tiles matches add them together
+                if ((matrix[x,y - i] == matrix[x,y - i + 1]) && (match == false))
+                {
+                    matrix[x, y - i] = matrix[x, y - i + 1] * 2;
+                    matrix[x, y - i + 1] = 0;
+                    match = true;
+                }
+                //If tile is blank move current blank there
+                else if (matrix[x, y - i] == 0)
+                {
+                    matrix[x, y - i] = matrix[x, y - i + 1];
+                    matrix[x, y - i + 1] = 0;
+                }
+                //If tile is not blank nothing happend
+
+                i += 1;
+                CheckUp(x, y, i, matrix, match); 
+            }
+        }
+
+        /// <summary>
+        /// Move all possible blocks down
+        /// </summary>
+        public void DownMove()
+        {
+            //Make copy of playing matrix
+            int[,] temporaryMatrix = PlayingMatrix;
+
+            //Check possible outcomes of the move
+            for (int y = PlayingMatrix.GetLength(1) - 2; y >= 0; y--)
+            {
+                for (int x = 0; x < PlayingMatrix.GetLength(0); x++)
+                {
+                    if (temporaryMatrix[x, y] != 0)
+                        CheckDown(x, y, 1, temporaryMatrix, false);
+                }
+            }
+
+            //Copy new data into PlayingMatrix
+            PlayingMatrix = temporaryMatrix;
+            SyncFields();
+        }
+
+        /// <summary>
+        /// Check downward tiles and move current tile if is it possible
+        /// </summary>
+        /// <param name="x">originalX</param>
+        /// <param name="y">originalY</param>
+        /// <param name="i">currentOffset</param>
+        /// <param name="matrix">temporaryMatrix</param>
+        /// <param name="match">if tiles was match</param>
+        private void CheckDown(int x, int y, int i, int[,] matrix, bool match)
+        {
+            if (y + i <= PlayingMatrix.GetLength(1) - 1)
+            {
+                //If tiles matches add them together
+                if ((matrix[x, y + i] == matrix[x, y + i - 1]) && (match == false))
+                {
+                    matrix[x, y + i] = matrix[x, y + i - 1] * 2;
+                    matrix[x, y + i - 1] = 0;
+                    match = true;
+                }
+                //If tile is blank move current blank there
+                else if (matrix[x, y + i] == 0)
+                {
+                    matrix[x, y + i] = matrix[x, y + i - 1];
+                    matrix[x, y + i - 1] = 0;
+                }
+                //If tile is not blank nothing happend
+
+                i += 1;
+                CheckDown(x, y, i, matrix, match);
+            }
+        }
+
+        /// <summary>
+        /// Move all possible blocks left
+        /// </summary>
+        public void LeftMove()
+        {
+            //Make copy of playing matrix
+            int[,] temporaryMatrix = PlayingMatrix;
+
+            //Check possible outcomes of the move
+            for (int y = 0; y < PlayingMatrix.GetLength(1); y++)
+            {
+                for (int x = 1; x < PlayingMatrix.GetLength(0); x++)
+                {
+                    if (temporaryMatrix[x, y] != 0)
+                        CheckLeft(x, y, 1, temporaryMatrix, false);
+                }
+            }
+
+            //Copy new data into PlayingMatrix
+            PlayingMatrix = temporaryMatrix;
+            SyncFields();
+        }
+
+        /// <summary>
+        /// Check left tiles and move current tile if is it possible
+        /// </summary>
+        /// <param name="x">originalX</param>
+        /// <param name="y">originalY</param>
+        /// <param name="i">currentOffset</param>
+        /// <param name="matrix">temporaryMatrix</param>
+        /// <param name="match">if tiles was match</param>
+        private void CheckLeft(int x, int y, int i, int[,] matrix, bool match)
+        {
+            if (x - i >= 0)
+            {
+                //If tiles matches add them together
+                if ((matrix[x - i, y] == matrix[x - i + 1, y]) && (match == false))
+                {
+                    matrix[x - i, y] = matrix[x - i + 1, y] * 2;
+                    matrix[x - i + 1, y] = 0;
+                    match = true;
+                }
+                //If tile is blank move current blank there
+                else if (matrix[x - i, y] == 0)
+                {
+                    matrix[x - i, y ] = matrix[x - i + 1, y];
+                    matrix[x - i + 1, y] = 0;
+                }
+                //If tile is not blank nothing happend
+
+                i += 1;
+                CheckLeft(x, y, i, matrix, match);
+            }
+        }
+
+        /// <summary>
+        /// Move all possible blocks right
+        /// </summary>
+        public void RightMove()
+        {
+            //Make copy of playing matrix
+            int[,] temporaryMatrix = PlayingMatrix;
+
+            //Check possible outcomes of the move
+            for (int y = 0; y < PlayingMatrix.GetLength(1); y++)
+            {
+                for (int x = PlayingMatrix.GetLength(0) - 2; x >= 0; x--)
+                {
+                    if (temporaryMatrix[x, y] != 0)
+                        CheckRight(x, y, 1, temporaryMatrix, false);
+                }
+            }
+
+            //Copy new data into PlayingMatrix
+            PlayingMatrix = temporaryMatrix;
+            SyncFields();
+        }
+
+        /// <summary>
+        /// Check right tiles and move current tile if is it possible
+        /// </summary>
+        /// <param name="x">originalX</param>
+        /// <param name="y">originalY</param>
+        /// <param name="i">currentOffset</param>
+        /// <param name="matrix">temporaryMatrix</param>
+        /// <param name="match">if tiles was match</param>
+        private void CheckRight(int x, int y, int i, int[,] matrix, bool match)
+        {
+            if (x + i <= PlayingMatrix.GetLength(0) - 1)
+            {
+                //If tiles matches add them together
+                if ((matrix[x + i, y] == matrix[x + i - 1, y]) && (match == false))
+                {
+                    matrix[x + i, y] = matrix[x + i - 1, y] * 2;
+                    matrix[x + i - 1, y] = 0;
+                    match = true;
+                }
+                //If tile is blank move current blank there
+                else if (matrix[x + i, y] == 0)
+                {
+                    matrix[x + i, y] = matrix[x + i - 1, y];
+                    matrix[x + i - 1, y] = 0;
+                }
+                //If tile is not blank nothing happend
+
+                i += 1;
+                CheckRight(x, y, i, matrix, match);
+            }
+        }
+    }
+}
---
+++ 2048/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2048
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly GameLogic gameLogic;
+        private TextBlock[,] PlayingField;
+
+        public MainWindow()
+        {
+            try
+            {
+                InitializeComponent();
+
+                gameLogic = new GameLogic(PopulatePlayingField());
+                SetScore();
+            }
+            catch
+            {
+                Error();
+            }
+        }
+
+        /// <summary>
+        /// Populate array with textblock
+        /// </summary>
+        /// <returns>PlayingField</returns>
+        private TextBlock[,] PopulatePlayingField()
+        {
+            PlayingField = new TextBlock[4, 4];
+
+            PlayingField[0, 0] = Block0_0;
+            PlayingField[0, 1] = Block0_1;
+            PlayingField[0, 2] = Block0_2;
+            PlayingField[0, 3] = Block0_3;
+            PlayingField[1, 0] = Block1_0;
+            PlayingField[1, 1] = Block1_1;
+            PlayingField[1, 2] = Block1_2;
+            PlayingField[1, 3] = Block1_3;
+            PlayingField[2, 0] = Block2_0;
+            PlayingField[2, 1] = Block2_1;
+            PlayingField[2, 2] = Block2_2;
+            PlayingField[2, 3] = Block2_3;
+            PlayingField[3, 0] = Block3_0;
+            PlayingField[3, 1] = Block3_1;
+            PlayingField[3, 2] = Block3_2;
+            PlayingField[3, 3] = Block3_3;
+
+            return PlayingField;
+        }
+
+        /// <summary>
+        /// Basic error message
+        /// </summary>
+        public void Error()
+        {
+            MessageBox.Show("Error. Pleasy try again", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        /// <summary>
+        /// Set score
+        /// </summary>
+        /// <param name="score">int with score</param>
+        public void SetScore()
+        {
+            ScoreLabel.Content = gameLogic.CountScore().ToString();
+        }
+
+        #region Events Handlers
+        //Event handler for keyDown
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                //Movement up
+                if ((e.Key == Key.W) || (e.Key == Key.Up))
+                    gameLogic.UpMove();
+
+                //Movement down
+                if ((e.Key == Key.S) || (e.Key == Key.Down))
+                    gameLogic.DownMove();
+
+                //Movement left
+                if ((e.Key == Key.A) || (e.Key == Key.Left))
+                    gameLogic.LeftMove();
+
+                //Movement right
+                if ((e.Key == Key.D) || (e.Key == Key.Right))
+                    gameLogic.RightMove();
+
+                //Restart
+                if (e.Key == Key.R)
+                    gameLogic.NewGame(PlayingField);
+
+                SetScore();
+            }
+            catch
+            {
+                Error();
+            }
+        }
+
+        //New Game with click on the button
+        private void NewGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                gameLogic.NewGame(PlayingField);
+                SetScore();
+            }
+            catch
+            {
+                Error();
+            }
+        }
+        #endregion
+    }
+}
